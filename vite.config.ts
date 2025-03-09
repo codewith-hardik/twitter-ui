5690af704415fd5d7ecd14012541074a0045a370
Hardik Chavda <chavdahardik1729@gmail.com>
2025-03-09 20:41:35 +0530

    Twitter UI

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  base: "/twitter-ui/",
   plugins: [
+
     tailwindcss(),
   ],
 })
